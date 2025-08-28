ac7b8999eeb61b6987ed8aa593751176b778ba9a
Veronique Galmiche <vgalmich@f2r2s7.clusters.42paris.fr>
2025-08-28 17:27:02 +0200

    cure done

---
+++ ex03/CureClass/Cure.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cure.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 17:22:39 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/28 17:26:29 by vgalmich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Cure.hpp"
+
+Cure::Cure() : AMateria("cure")
+{
+    std::cout << _type << " has been generated successfully !" << std::endl;
+}
+
+Cure::Cure(const Cure &copy) : AMateria(copy)
+{
+    std::cout << "A copy of " << _type << " has been generated successfully !" << std::endl;
+}
+
+Cure &Cure::operator=(const Cure &other)
+{
+    if (this != &other)
+        AMateria::operator=(other);
+    std::cout << _type << " assignment operator has been called." << std::endl;
+    return (*this);
+}
+
+Cure::~Cure()
+{
+    std::cout << _type << " has been vasnished." << std::endl;
+}
+
+AMateria *Cure::clone() const
+{
+    return (new Cure(*this));
+}
+
+void Cure::use(ICharacter &target)
+{
+    std::cout << "* A soothing light heals " << target.getName() << "! *" << std::endl;
+}
---
+++ ex03/CureClass/Cure.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Cure.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 17:06:31 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/28 17:22:18 by vgalmich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CURE_HPP
+#define CURE_HPP
+
+#include "AMateria.hpp"
+#include "../ICharacterClass/ICharacter.hpp"
+
+class Cure : public AMateria
+{
+    public:
+        Cure();
+        Cure(const Cure &copy);
+        Cure &operator=(const Cure &other);
+        ~Cure();
+
+        virtual AMateria* clone() const ;
+        virtual void use(ICharacter &target);
+};
+
+#endif
